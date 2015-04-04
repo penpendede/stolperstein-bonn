f8c92ec86040789a3889b3eaa911d299aa38ae25
Josef Schugt <penpendede@gmail.com>
2015-04-04 12:29:43 +0200

    Ruby-Skript zum Erzeugen der JSON-Datei verwendet jetzt Rubys native JSON-Serialisierung.

--- create_JSON_file.rb
+++ create_JSON_file.rb
@@ -1,62 +1,46 @@
 #!/usr/bin/env ruby
 require 'sqlite3'
+require 'json'
 
 targetFilename='page/stolperstein.json'
 
-fields = [
-    'lon',
-    'lat',
-    'Geborene',
-    'Jahrgang',
-    'Vorname',
-    'Familienname',
-    'Ortsbeschreibung',
-    'Bezirksname',
-    ].join(', ')
-
-source = [
-    'TOpfer',
-    'TStolperstein',
-    'TStadtbezirk'
-    ].join(' NATURAL JOIN ')
-
-orderby = [
-    'Familienname',
-    'Vorname'
-    ].join(', ')
-query = "select #{fields} FROM #{source} ORDER BY #{orderby}"
-
+query =
+    'SELECT     lon,'                           + "\n" +
+    '           lat,'                           + "\n" +
+    '           Geborene,'                      + "\n" +
+    '           Jahrgang,'                      + "\n" +
+    '           Vorname,'                       + "\n" +
+    '           Familienname,'                  + "\n" +
+    '           Ortsbeschreibung,'              + "\n" +
+    '           Bezirksname'                    + "\n" +
+    'FROM       TOpfer          NATURAL JOIN'   + "\n" +
+    '           TStolperstein   NATURAL JOIN'   + "\n" +
+    '           TStadtbezirk'                   + "\n" +
+    'ORDER BY   Familienname,'                  + "\n" +
+    '           Vorname'
 
 def feature(row)
-    result = [];
     lon, lat, geborene, jahrgang, vorname, familienname, ort, bezirk = row
-
-    result = [
-        "\t\t{",
-        "\t\t\t\"type\": \"Feature\",",
-        "\t\t\t\"geometry\":",
-        "\t\t\t{",
-        "\t\t\t\t\"type\": \"Point\",",
-        "\t\t\t\t\"coordinates\": [#{lon}, #{lat}]",
-        "\t\t\t},",
-        "\t\t\t\"properties\":",
-        "\t\t\t{",
-        ]
+    result =
+        {
+            'type'          => 'Feature',
+            'geometry'      => {
+                'type'          => 'Point',
+                'coordinates'   => [lon, lat]
+                },
+            'properties'    => {
+                'ort'           => ort + ', ' +bezirk,
+                'vorname'       => vorname,
+                'familienname'  => familienname
+                }
+            }
     unless geborene.nil?
-        result.push("\t\t\t\t\"geborene\": \"#{geborene}\",")
+        result['properties']['geborene'] = geborene
     end
     unless jahrgang.nil?
-        result.push("\t\t\t\t\"jahrgang\": #{jahrgang},")
+        result['properties']['jahrgang'] = jahrgang
     end
-    result += [
-        "\t\t\t\t\"vorname\": \"#{vorname}\",",
-        "\t\t\t\t\"familienname\": \"#{familienname}\",",
-        "\t\t\t\t\"ort\": \"#{ort}, #{bezirk}\"",
-        "\t\t\t}",
-        "\t\t}"
-        ]
-
-    result.join("\n");
+    result
 end
 
 db = SQLite3::Database.new("stolperstein.sqlite3")
@@ -65,11 +49,9 @@ db.execute(query) do |row|
     features.push(feature(row))
 end
 
-
 File.open(targetFilename, 'w') do |target|
-    target.puts "{ \"type\": \"FeatureCollection\","
-    target.puts "\t\"features\": ["
-    target.puts features.join(",\n")
-    target.puts "\t]"
-    target.puts "}"
+    target.puts({
+        'type'      => 'FeatureCollection',
+        'features'  =>  features
+        }.to_json)
 end
