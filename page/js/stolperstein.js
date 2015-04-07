a9aad1fb72a35674652d9949121eab09a6d8098f
Josef Schugt <penpendede@gmail.com>
2015-04-07 20:18:09 +0200

    No more javascript in index.html

---
+++ page/js/stolperstein.js
@@ -0,0 +1,45 @@
+/*jslint browser:true*/
+/*jslint unparam:true*/
+/*global $, jQuery, L*/
+
+var map = L.map('map', {
+    center: [50.7085234, 7.115605],
+    zoom: 12,
+    maxZoom: 19,
+    fullscreenControl: true
+});
+L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "stolperstein.json",
+    success: function (data) {
+        "use strict";
+        var lat, lon, props, description, opfer, marker;
+        $(data.features).each(function (key, data) {
+            lat = data.geometry.coordinates[0];
+            lon = data.geometry.coordinates[1];
+            props = data.properties;
+            description = [ '<table style="max-width: 80%;">' ];
+            opfer = props.vorname + ' ' + props.familienname;
+            if (props.geborene) {
+                opfer += " geb. " + props.geborene;
+            }
+            if (props.jahrgang) {
+                opfer += ", Jahrgang " + props.jahrgang;
+            }
+            description.push("<tr><th>Opfer:</th><td>" + opfer + "</td></tr>");
+            description.push("<tr><th>Ort:</th><td>" + props.ort + "</td></tr>");
+            description.push("<tr><th>Verlegejahr:</th><td>" + props.verlegejahr + "</td></tr>");
+            description.push("</table>");
+            marker = L.marker([lon, lat]);
+            marker.bindPopup(description.join(''));
+            marker.addTo(map);
+        });
+    }
+}).error(function () {
+    "use strict";
+    return;
+});
