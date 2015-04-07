0953b51359eddc7eed4d06927c65b092675b5f83
Josef Schugt <penpendede@gmail.com>
2015-04-07 21:33:46 +0200

    Clustering hinzugefügt, jetzt stellen identische Adressen kein Problem mehr dar.

--- page/js/stolperstein.js
+++ page/js/stolperstein.js
@@ -12,32 +12,37 @@ L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+var markers = L.markerClusterGroup();
+
 $.ajax({
     dataType: "json",
     url: "stolperstein.json",
-    success: function (data) {
+    success: function (geoJsonData) {
         "use strict";
-        var lat, lon, props, description, opfer, marker;
-        $(data.features).each(function (key, data) {
-            lat = data.geometry.coordinates[0];
-            lon = data.geometry.coordinates[1];
-            props = data.properties;
-            description = [ '<table>' ];
-            opfer = props.vorname + ' ' + props.familienname;
-            if (props.geborene) {
-                opfer += " geb. " + props.geborene;
+        var geoJsonLayer = L.geoJson(geoJsonData, {
+            onEachFeature: function (feature, layer) {
+                var props, description, opfer;
+                layer.bindPopup(feature.properties.address);
+                props = feature.properties;
+                description = [ '<table>' ];
+                opfer = props.vorname + ' ' + props.familienname;
+                if (props.geborene) {
+                    opfer += " geb. " + props.geborene;
+                }
+                if (props.jahrgang) {
+                    opfer += "<br />Jahrgang " + props.jahrgang;
+                }
+                description.push("<tr><th>Opfer:</th><td>" + opfer + "</td></tr>");
+                description.push("<tr><th>Ort:</th><td>" + props.ort + "</td></tr>");
+                description.push("<tr><th>Verlegejahr:</th><td>" + props.verlegejahr + "</td></tr>");
+                description.push("</table>");
+                layer.bindPopup(description.join(''));
             }
-            if (props.jahrgang) {
-                opfer += "<br />Jahrgang " + props.jahrgang;
-            }
-            description.push("<tr><th>Opfer:</th><td>" + opfer + "</td></tr>");
-            description.push("<tr><th>Ort:</th><td>" + props.ort + "</td></tr>");
-            description.push("<tr><th>Verlegejahr:</th><td>" + props.verlegejahr + "</td></tr>");
-            description.push("</table>");
-            marker = L.marker([lon, lat]);
-            marker.bindPopup(description.join(''));
-            marker.addTo(map);
         });
+        markers.addLayer(geoJsonLayer);
+        markers.addLayer(geoJsonLayer);
+        map.addLayer(markers);
+
     }
 }).error(function () {
     "use strict";
