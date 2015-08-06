8dd61326993056d03fdf2bb96ac1fd02b68a823e
Josef Schugt <josef.schugt@benndorf.de>
2015-08-06 02:04:21 +0200

    Neue Version, Daten komplett aus OpenStreetMap

--- README.md
+++ README.md
@@ -6,20 +6,11 @@ die Bonner Stolpersteine in einem möglichst offenen Format digital
 aufzubereiten. Auf diese Weise sollen die Schicksale, für die diese
 Stolpersteine stehen, auch in der der virtuellen Datenwelt präsent werden.
 
-Ausgangspunkt sind SQL-Skripte, die eine SQLite3-Datenbank befüllen. Ein
-Ruby-Skript führt die einzelnen SQL-Skripte in der richtigen Reihenfolge
-zusammen. Die Reihenfolge ist wichtig, da die SQL-Skripte zum Verhindern von
-Fehlern und Inkonsistenzen die Abhängigkeiten der Daten erzwingen.
-
-Aus der Datenbank wird dann mit Hilfe eines weiteren Ruby-Skripts eine
-GeoJSON-Datei erzeugt, die anschließend von einer Internetseite mit Hilfe von
-Leaflet durch Marker auf einer Karte Bonns angezeigt wird. Beim Anklicken des
-Markers erscheinen die zugehörigen Informationen in einem Popup-Fenster.
+Gegenüber der ursprünglichen Fassung gibt es eine erhebliche Veränderung:
+Die Daten werden jetzt direkt von OpenStreetMap abgerufen. Das hat den Vorteil,
+dass jederzeit auf aktuelle Daten zurückgegriffen wird.
 
 ![Screenshot](files/screenshot.png)
-
-Das Repository enthält zudem eine fertige SQLite3-Datenbank mit den bislang
-erfassten Stolperstein-Daten.
 
 ## Demo
 
--- page/js/stolperstein.js
+++ page/js/stolperstein.js
@@ -1,49 +1,109 @@
-/*jslint browser:true*/
-/*jslint unparam:true*/
-/*global $, jQuery, L*/
+/*jslint browser:true, unparam:true*/
+/*global $, jQuery, L, osmtogeojson*/
 
-var map = L.map('map', {
-    center: [50.7085234, 7.115605],
-    zoom: 12,
-    maxZoom: 19,
-    fullscreenControl: true
-});
-L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-var markers = L.markerClusterGroup();
-
-$.ajax({
-    dataType: "json",
-    url: "stolperstein.json",
-    success: function (geoJsonData) {
-        "use strict";
-        var geoJsonLayer = L.geoJson(geoJsonData, {
-            onEachFeature: function (feature, layer) {
-                var props, description, opfer;
-                layer.bindPopup(feature.properties.address);
-                props = feature.properties;
-                description = [ '<table>' ];
-                opfer = props.vorname + ' ' + props.familienname;
-                if (props.geborene) {
-                    opfer += " geb. " + props.geborene;
-                }
-                if (props.jahrgang) {
-                    opfer += "<br />Jahrgang " + props.jahrgang;
-                }
-                description.push("<tr><th>Opfer:</th><td>" + opfer + "</td></tr>");
-                description.push("<tr><th>Ort:</th><td>" + props.ort + "</td></tr>");
-                description.push("<tr><th>Verlegejahr:</th><td>" + props.verlegejahr + "</td></tr>");
-                description.push("</table>");
-                layer.bindPopup(description.join(''));
+$(document).ready(
+    function () {
+        var map = L.map(
+            'map',
+            {
+                center: [50.7085234, 7.115605],
+                maxBounds: [[50.63, 7.01], [50.78, 7.22]],
+                zoom: 12,
+                maxZoom: 19,
+                fullscreenControl: true
             }
-        });
-        markers.addLayer(geoJsonLayer);
-        map.addLayer(markers);
+        );
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png',
+            {
+                attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                useCache: true
+            }
+        ).addTo(map);
 
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.se/hydda/roads_and_labels/{z}/{x}/{y}.png',
+            {
+                attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                useCache: true
+            }
+        ).addTo(map);
+
+        var markers = L.markerClusterGroup();
+
+        var popup = L.popup().setLatLng([50.7085234, 7.115605]).setContent('<h3 style="text-align:center;">Einen Moment bitte</h3><p style="text-align:center;">Die aktuellen Stolperstein-Informationen werden bei OpenStreetMap abgefragt.</p>').openOn(map);
+        $.ajax(
+            {
+                dataType: 'json',
+                datatype: 'html',
+                url: 'http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(3600062508)->.area;(node["memorial:type"="stolperstein"](area.area);way["memorial:type"="stolperstein"](area.area);relation["memorial:type"="stolperstein"](area.area););out meta;>;out meta qt;',
+                success: function (jsonData) {
+                    'use strict';
+                    map.closePopup(popup);
+                    var geoJsonData = osmtogeojson(jsonData);
+                    var geoJsonLayer = L.geoJson(
+                        geoJsonData,
+                        {
+                            onEachFeature: function (feature, layer) {
+                                var tags = feature.properties.tags;
+                                var description = [ '<table>' ];
+                                var ort = '';
+
+                                if (tags.name) {
+                                    if (tags['addr:street']) {
+                                        ort = tags['addr:street'];
+
+                                        if (tags['addr:housenumber']) {
+                                            ort = ort + ' ' + tags['addr:housenumber'];
+                                        }
+                                    }
+                                    if (tags['addr:city']) {
+                                        if (ort) {
+                                            ort = ort + ', ';
+                                        }
+                                        if (tags['addr:postcode']) {
+                                            ort = ort + tags['addr:postcode'] + ' ';
+                                        }
+                                        ort = ort + tags['addr:city'];
+                                    }
+
+
+                                    description.push('<tr><th>' + tags.name + '</th></tr>');
+                                    if (ort) {
+                                        description.push('<tr><td>' + ort + '</td></tr>');
+                                    }
+                                    if (tags['memorial:text']) {
+                                        description.push('<tr><td>' + tags['memorial:text'] + '</td></tr>');
+                                    }
+                                    description.push('</table>');
+                                    if (tags.image) {
+                                        description.push(
+                                            '<a href="' +
+                                            tags.image +
+                                            '" target="_blank"><img width="300" height="225" src="' +
+                                            tags.image.replace(
+                                                'https://upload.wikimedia.org/wikipedia/commons',
+                                                'https://upload.wikimedia.org/wikipedia/commons/thumb'
+                                            ) +
+                                            '/300px-' +
+                                            tags.image.split('/')[tags.image.split('/').length - 1] +
+                                            '" /></a>'
+                                        );
+                                    }
+
+                                    layer.bindPopup(feature.properties.address);
+                                    layer.bindPopup(description.join(''));
+                                }
+                            }
+                        }
+                    );
+                    markers.addLayer(geoJsonLayer);
+                    map.addLayer(markers);
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    popup.setContent('<h3 style="text-align:center;">Leider konnten die Daten der Stolpersteine nicht geladen werden.</h3>');
+                }
+            }
+        );
     }
-}).error(function () {
-    "use strict";
-    return;
-});
+);
