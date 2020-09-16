ef6daa9a72564b8cddc84cee5b841dd5de88f722
Josef Schugt <penpendede@mail.ru>
2020-09-16 23:04:39 +0200

    added fetch script

---
+++ scripts/fetch.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
+
+const tasks = [
+  {
+    src: 'https://stadtplan.bonn.de/geojson?Thema=21248',
+    name: 'stadtgebiet',
+    info: 'Fläche Stadtgebiet Bonn'
+  },
+  {
+    src: 'https://stadtplan.bonn.de/geojson?Thema=14574',
+    name: 'bezirke',
+    info: 'Flächen der Stadtbezirke'
+  },
+  {
+    src: 'https://stadtplan.bonn.de/geojson?Thema=21247',
+    name: 'ortsteile',
+    info: 'Flächen der Ortsteile'
+  }
+]
+
+tasks.forEach(task => {
+  performTask(task)
+})
+
+function performTask (task) {
+  https.get(task.src, resp => {
+    let data = 'window.stolpersteins.' + task.name + ' = '
+    resp.on('data', chunk => {
+      data += chunk
+    })
+    resp.on('end', () => {
+      fs.writeFile(path.join('files', task.name + '.js'), data, err => {
+        if (err) {
+          return console.log(err)
+        }
+        console.log(task.info)
+      })
+    })
+  }).on('error', (err) => {
+    console.log('Fehler: ' + err.message)
+  })
+}
