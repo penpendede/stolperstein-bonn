d560de55602aa88773e52f95c9bac02bc7cfd7c4
Josef Schugt <penpendede@mail.ru>
2020-09-26 17:16:22 +0200

    fetch tasks now in fetch_tasks.json, fetch.js with better output

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -3,44 +3,38 @@ const fs = require('fs')
 const https = require('https')
 const path = require('path')
 
-const tasks = [
-  {
-    src: 'https://stadtplan.bonn.de/geojson?Thema=21248',
-    name: 'stadtgebiet',
-    info: 'Fläche Stadtgebiet Bonn'
-  },
-  {
-    src: 'https://stadtplan.bonn.de/geojson?Thema=14574',
-    name: 'bezirke',
-    info: 'Flächen der Stadtbezirke'
-  },
-  {
-    src: 'https://stadtplan.bonn.de/geojson?Thema=21247',
-    name: 'ortsteile',
-    info: 'Flächen der Ortsteile'
-  }
-]
+const tasks = JSON.parse(fs.readFileSync('fetch_tasks.json'))
 
 tasks.forEach(task => {
-  performTask(task)
+  switch (task.type) {
+    case 'download':
+      download(task)
+      break
+  }
 })
 
-function performTask (task) {
-  https.get(task.src, resp => {
-    resp.setEncoding('utf-8')
-    let data = 'window.stolpersteine.data.' + task.name + ' = '
-    resp.on('data', chunk => {
-      data += chunk
-    })
-    resp.on('end', () => {
-      fs.writeFile(path.join('js', task.name + '.js'), data, err => {
-        if (err) {
-          return console.log(err)
-        }
-        console.log(task.info)
+function download (task) {
+  try {
+    https.get(task.args.source, resp => {
+      resp.setEncoding('utf-8')
+      let data = 'window.stolpersteine.data.' + task.args.target + ' = '
+      resp.on('data', chunk => {
+        data += chunk
       })
+      resp.on('end', () => {
+        fs.writeFile(path.join('js', task.args.target + '.js'), data, err => {
+          if (err) {
+            return console.error(err)
+          }
+          console.log(chalk.greenBright.bold('[OKAY]') + ' ' + task.info)
+        })
+      })
+    }).on('error', (error) => {
+      console.log(chalk.redBright.bold('[FAIL]') + ' ' + task.info)
+      console.error(error.message)
     })
-  }).on('error', (err) => {
-    console.log('Fehler: ' + err.message)
-  })
+  } catch (error) {
+    console.log(chalk.redBright.bold('[FAIL]') + ' ' + task.info)
+    console.error(error.message)
+  }
 }
