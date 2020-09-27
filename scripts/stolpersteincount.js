f52c27a741a502e99b44dfee84c6336b3a997b8b
Josef Schugt <penpendede@mail.ru>
2020-09-27 23:51:13 +0200

    count stolpersteins in ortsteil, bezirk

---
+++ scripts/stolpersteincount.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const JSON5 = require('json5')
+const insidePolygon = require('point-in-polygon')
+
+const ortsteile = JSON5.parse( // parse as JSON5
+  fs.readFileSync('js/ortsteile.js', 'utf8') // content of ortsteile.js
+    .replace(/^[^=]*=\s*/, '') // without the leading assingment
+)
+const stolpersteine = JSON5.parse( // parse as JSON5
+  fs.readFileSync('js/stolpersteine.js', 'utf8') // content of ortsteile.js
+    .replace(/^[^=]*=\s*/, '') // without the leading assingment
+)
+
+const ortsteilCount = {}
+const bezirkCount = {}
+
+const ortsteilClassification = []
+ortsteile.features.forEach(feature => {
+  const geometry = feature.geometry.coordinates
+  const ortsteil = feature.properties.ortsteil_bez
+  const bezirk = feature.properties.bezirk_bez
+  ortsteilClassification.push({
+    geometry: geometry[0],
+    ortsteil: ortsteil,
+    bezirk: bezirk
+  })
+  ortsteilCount[ortsteil] = 0
+  bezirkCount[bezirk] = 0
+})
+
+const stolpersteinPositions = []
+stolpersteine.elements.forEach(element => {
+  stolpersteinPositions.push([element.lon, element.lat])
+})
+
+stolpersteinPositions.forEach(stolpersteinPosition => {
+  ortsteilClassification.forEach(ortsteil => {
+    if (insidePolygon(stolpersteinPosition, ortsteil.geometry)) {
+      ortsteilCount[ortsteil.ortsteil] += 1
+      bezirkCount[ortsteil.bezirk] += 1
+    }
+  })
+})
+console.log(ortsteilCount)
+console.log(bezirkCount)
+// const chalk = require('chalk')
