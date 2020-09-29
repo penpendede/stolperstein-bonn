0b03b0cc532b84fe3974b49f85ab7de2fa8731c1
Josef Schugt <penpendede@mail.ru>
2020-09-29 20:48:11 +0200

    clustering is back

--- js/addStolpersteine.js
+++ js/addStolpersteine.js
@@ -1,4 +1,8 @@
 window.stolpersteine.fn.addStolpersteine = function () {
   var geojson = window.osmtogeojson(window.stolpersteine.data.stolpersteine)
-  window.L.geoJson(geojson).addTo(window.stolpersteine.map)
+  var markers = window.L.markerClusterGroup({
+    maxClusterRadius: 50
+  })
+  markers.addLayer(window.L.geoJson(geojson))
+  window.stolpersteine.map.addLayer(markers)
 }
