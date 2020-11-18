cddbf64d640cabebc374133fa6d8c4584ac3ae06
Josef Schugt <penpendede@mail.ru>
2020-11-18 22:15:55 +0100

    updated stolperstein data

--- js/lastModified.js
+++ js/lastModified.js
@@ -3,7 +3,7 @@ window.stolpersteine.data.lastModified = [
     id: 'overview',
     description: 'Diese Übersicht',
     file: '',
-    modified: '2020-11-1 18:44:07'
+    modified: '2020-11-18 22:13:44'
   },
   {
     id: 'stadtgebiet',
@@ -27,24 +27,24 @@ window.stolpersteine.data.lastModified = [
     id: 'stolpersteine',
     description: 'Stolpersteine',
     file: 'js/stolpersteine.js',
-    modified: '2020-11-1 18:42:46'
+    modified: '2020-11-18 22:13:16'
   },
   {
     id: 'zusatzdaten',
     description: 'Zusatzdaten',
     file: 'js/zusatzdaten.js',
-    modified: '2020-10-20 21:04:02'
+    modified: '2020-11-16 22:04:44'
   },
   {
     id: 'bezirkCount',
     description: 'Anzahl der Stolpersteine in den einzelnen Stadtbezirken',
     file: 'js/bezirkCount.js',
-    modified: '2020-11-1 18:44:02'
+    modified: '2020-11-18 22:13:34'
   },
   {
     id: 'ortsteilCount',
     description: 'Anzahl der Stolpersteine in den einzelnen Ortsteilen',
     file: 'js/ortsteilCount.js',
-    modified: '2020-11-1 18:44:02'
+    modified: '2020-11-18 22:13:34'
   }
 ]
